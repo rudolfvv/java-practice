9f05fb139092bebfa50f2cc945deb49dd93e0059
Vadim Rudolf <vadimkarudolf0801@mail.ru>
2026-06-15 02:42:30 +0700

    Добавил трекер успеваемости учеников

---
+++ StudentTracker.java
@@ -0,0 +1,22 @@
+public class StudentTracker {
+    public static void main(String[] args) {
+        int[] grades = {5, 4, 3, 5, 4, 5, 3, 4}; 
+        
+        double sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        
+        double average = sum / grades.length;
+        
+        System.out.println("--- Анализ успеваемости ученика ---");
+        System.out.println("Всего выставлено оценок: " + grades.length);
+        System.out.println("Средний балл: " + String.format("%.2f", average));
+        
+        if (average >= 4.0) {
+            System.out.println("Статус: Отличный результат, допущен к экзамену!");
+        } else {
+            System.out.println("Статус: Внимание! Требуются дополнительные занятия.");
+        }
+    }
+}
